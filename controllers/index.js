f91bbc0d72d253dcb14673a698ad7862f08fddbf
Sarah Tanner <sktanner92@gmail.com>
2021-07-10 16:51:14 -0400

    Tested character endpoints, all working except delete

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,4 @@
 module.exports ={
     userController: require('./userController.js'),
-    charController: require('./charactercontroller')
+    charController: require('./charactercontroller.js')
 }
