a958bc20e4a8dcd11e3c592cd2aa0a49ae956ece
Brad Snyder <bsnyder13@gmail.com>
2021-07-21 21:49:52 -0400

    no changes

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
 
-const sequelize = new Sequelize("postgres://postgres:afd60085930c46f0923426a39dc78dfc@localhost:5432/bbproject")
+const sequelize = new Sequelize("postgres://postgres:EFA2021!@localhost:5432/bbproject")
 
 
 module.exports = sequelize
